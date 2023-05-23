9ba9db756b931cb787ba43463e700c4519b07f1b
Thaleschavesdev2 <113631715+Thaleschavesdev2@users.noreply.github.com>
2023-05-23 16:45:26 -0300

    add aulas

---
+++ MeuSuperBanco/contabanco.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuSuperBanco
+{
+    public class Contabanco
+    {
+        public string Numero { get; set; }
+        public string Dono { get; set; }
+        public decimal Saldo
+        {
+            get
+            {
+                decimal saldo = 10000;
+                foreach (var item in todasTransacoes)
+                {
+
+                    saldo += item.Valor;
+                }
+                return saldo;
+            }
+
+        }
+
+        public static int numeroConta = 123456789;
+
+        private List <Transacao> todasTransacoes = new List <Transacao>();
+
+
+
+
+        public Contabanco(string nome, decimal valor)
+        {
+            this.Dono = nome;
+           
+
+            numeroConta++;
+
+            this.Numero = numeroConta.ToString();
+
+        }
+        public void Depositar( decimal valor,DateTime data,string obs)
+
+        {
+            if (valor<=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(valor),"Nao aceitamos valor deposito de valor menor que 0");
+            }
+
+            Transacao trans = new Transacao(valor, data, obs);
+            todasTransacoes.Add(trans);
+
+        }
+        public void Sacar(decimal valor, DateTime data, string obs)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(valor), "Nao aceitamos valor saque de valor menor que 0");
+            }
+            if (Saldo - valor < 0)
+            {
+                throw new ArgumentOutOfRangeException( "Voce nao tem saldo");
+            }
+            Transacao trans = new Transacao(-valor, data, obs);
+            todasTransacoes.Add(trans);
+        
+        }
+        public string PegarMovimentacao()
+        {
+            var movimentacoes = new StringBuilder();
+            movimentacoes.AppendLine("Data\t\t Valor\t\t Obs");
+
+            foreach (var item in todasTransacoes)
+            {
+
+                movimentacoes.AppendLine($"{item.date.ToShortDateString()}\t {item.Valor}\t {item.Obs}");
+            }
+            return movimentacoes.ToString();
+
+
+        }
+    
+    
+    
+    }
+}
+
---
+++ dedaticoRedimencionador/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace didaticos.redimicionador
+{
+   internal class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Iniciando rediminsionador1");
+
+            Thread thread = new Thread(Redimensionar);
+            thread.Start();
+
+            Console.Read();
+             }
+        static void Redimensionar()
+        {
+            string diretorio_entrada = "Arquivo_entrada";
+            string diretorio_finalidade = "Arquivo_Finalidade";
+            string diretorio_redimensionado = "Arquivo_Redimensionamento";
+
+            if (!Directory.Exists(diretorio_entrada))
+            {
+                Directory.CreateDirectory(diretorio_entrada);
+
+            }
+            
+            if (!Directory.Exists(diretorio_finalidade))
+            {
+                Directory.CreateDirectory(diretorio_finalidade);
+
+            }
+            
+            if (!Directory.Exists(diretorio_redimensionado))
+            {
+                Directory.CreateDirectory(diretorio_redimensionado);
+
+            }
+            while (true)
+            {
+                Thread.Sleep(new TimeSpan(0, 0, 5));
+            }
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+}
+
+
+
